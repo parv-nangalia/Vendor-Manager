e2ea4d925470851f269b7151429138887b103b68
parv-nangalia <nangaliaparv@gmail.com>
2024-05-05 21:49:20 +0530

    main_app, serializers

---
+++ Vendor_Manager/main/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Vendor, PurchaseOrder, Performance
+
+class VendorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vendor
+        fields = 'id', 'name', 'contact_details' ,'address'
+
+
+class POSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PurchaseOrder
+        fields = '__all__'
+
+
+class PerformanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Performance
+        fields = '__all__'
+
