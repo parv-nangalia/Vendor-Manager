549c869bc74ca007d3a3605f2ca0a5a104b75121
parv-nangalia <nangaliaparv@gmail.com>
2024-05-08 04:15:16 +0530

    Tests Bugfix

--- Vendor_Manager/main/tests.py
+++ Vendor_Manager/main/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from .models import Vendor, PurchaseOrder, Performance
@@ -8,7 +9,10 @@ from django.utils import timezone
 
 class VendorAPITestCase(TestCase):
     def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
         self.client = APIClient()
+        self.client.login(username='testuser', password='testpassword')
+
         self.vendor1 = Vendor.objects.create(name='Vendor 1', contact_details='9999999999')
         self.vendor2 = Vendor.objects.create(name='Vendor 2', contact_details='8888899999')
 
@@ -16,7 +20,7 @@ class VendorAPITestCase(TestCase):
         url = reverse('api-vendor')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)
+        self.assertEqual(len(response.data["vendor_data"]), 2)
 
         data = {'name': 'New Vendor', 'contact_details': '9999944774', 'address':'India'}
         response = self.client.post(url, data, format='json')
@@ -33,14 +37,19 @@ class VendorAPITestCase(TestCase):
 
 class PurchaseOrderAPITestCase(TestCase):
     def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
         self.client = APIClient()
+        self.client.login(username='testuser', password='testpassword')
         self.vendor = Vendor.objects.create(name='Vendor', contact_details='9999999999')
         self.po1 = PurchaseOrder.objects.create(vendor=self.vendor, order_date= '2024-05-05 15:30:00+05:30' , delivery_date= '2024-05-05 15:30:00+05:30', items= 'raw',quantity= 5,status='pending')
         self.po2 = PurchaseOrder.objects.create(vendor=self.vendor, order_date= '2024-05-05 15:30:00+05:30' , delivery_date= '2024-05-05 15:30:00+05:30', items= 'fresh',quantity= 8,status='pending')
 
     def test_purchase_order_create_api(self):
         url = reverse('api-po')
-        print(self.po1.items, self.po2.items)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data["purchase_orders"]), 2)
+       # print(self.po1.items, self.po2.items)
         data = {'vendor': self.vendor.id,'order_date': '2024-05-05 15:30:00+05:30', 'delivery_date': '2024-05-05 15:30:00+05:30', 'items': 'raw', 'quantity': '5', 'status': 'pending'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -56,7 +65,9 @@ class PurchaseOrderAPITestCase(TestCase):
 
 class VendorPerformanceAPITestCase(TestCase):
     def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
         self.client = APIClient()
+        self.client.login(username='testuser', password='testpassword')
         self.vendor = Vendor.objects.create(name='Vendor', contact_details='9999998888')
         self.performance1 = Performance.objects.create(vendor=self.vendor, on_time_delivery=0.8, date=timezone.now(), quality_rating_avg= 0.0, average_response_time= 0.0, fulfillment_rate = 0.0)
         self.performance2 = Performance.objects.create(vendor=self.vendor, on_time_delivery=0.9, date=timezone.now(), quality_rating_avg= 0.0, average_response_time= 0.0, fulfillment_rate = 0.0)
